4dd93d132c561cbeff01f5eacca4d2827d6b33ee
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-07-18 23:15:44 +0200

    item fix

--- scripts/utils/LnKutils.js
+++ scripts/utils/LnKutils.js
@@ -7,16 +7,18 @@ function Translate(pName){
 
 class GeometricUtils {
 	//DELCARATIONS
-	static createKeyItem(pName = Translate("Word.Key")) {} // creates new key item and returns the document
+	static async createKeyItem(pName = Translate("Word.Key")) {} // creates new key item and returns the document
 	
 	//IMPLEMENTATIONS
-	static createKeyItem(pName = Translate("Word.Key")) {
-		let vDocument =  game.items.createDocument({name : pName, type : "equipment", img:"icons/sundries/misc/key-steel.webp"});
+	static async createKeyItem(pName = Translate("Word.Key")) {
+		let vDocument =  game.items.createDocument({name : pName, type : "equipment", img:"icons/sundries/misc/key-steel.webp"});	
 		
-		vDocument.constructor.create(vDocument);
-		
-		return vDocument;
+		return await vDocument.constructor.create(vDocument);
 	}
 }
+
+Hooks.on("ready", function() {
+	GeometricUtils.createKeyItem();
+});
 
 export { cModuleName, Translate, GeometricUtils }
