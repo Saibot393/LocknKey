2a88258c14907af686f961eb0a36a43f95f75e0f
p4535992 <tentimarco0@gmail.com>
2023-12-02 22:29:21 +0100

    update api documentation

--- scripts/compatibility/APIHandler.js
+++ scripts/compatibility/APIHandler.js
@@ -47,8 +47,8 @@ export const API = {
 	 * @param {string} pUserID The game user id associated to the actor to whom transfer the item
 	 * @param {TokenDocument} pInventoryOwner The token document reference to the actor target (the one ownings the items) 
      * @param {Object} options generic object contains all the options
-     * @param {string} [options.customHeader=""]  The custom header
-     * @param {string} [options.TakerID=""]  The taker id is the game user id of the taker
+     * @param {string} [options.customHeader=""]  The custom header to change header of loot inventory (can include HTML)
+     * @param {string} [options.TakerID=""]  The taker id to transfer loot to specific token
      * @returns {void}
 	 */
     openTIWindowfor(pUserID, pInventoryOwner, pOptions = {customHeader : "", TakerID : ""}) {
@@ -73,6 +73,92 @@ export const API = {
 	 */
     async CheckActorPTokensCompatibility() {
 		return await CheckActorPTokensCompatibility();
+	},
+
+	/**
+	 * Updates the Lock & Key formula world settings with the newest version
+	*/
+	ResettoStandardFormulas({pLP = true, pLB = true}) {
+		return game.LocknKey.ResettoStandardFormulas({pLP, pLB});
+	},
+
+	/* ========================= */
+	/* GM API */
+	/* ========================= */
+
+	/** 
+	 * Copy hovered lock
+     */
+	CopyhoveredLockGM() {
+		return game.LocknKey.CopyhoveredLockGM();
+	},
+
+	/**
+	 * Create new key for hovered lock
+	 */
+	CreateNewKeyhoveredGM() {
+		return game.LocknKey.CreateNewKeyhoveredGM();
+	},
+
+	/**
+	 * Paste hovered lock
+	 */
+	PastehoveredLockGM() {
+		return game.LocknKey.PastehoveredLockGM();
+	},
+
+	/** 
+	 * Reset Pick Pocket DC (selected or scene wide) to the default value
+	 * @param {TokenDocument} token
+	 */
+	ResetPickPocketDC(token) {
+		return game.modules.get("LocknKey").api.LnKFlags.ResetPickPocketDC(token);
+	},
+
+	/**
+	 * Toggle hovered lock
+	 */
+	TogglehoveredLockGM() {
+		return game.LocknKey.TogglehoveredLockGM();
+	},
+
+	/* ========================= */
+	/* PLAYER API */
+	/* ========================= */
+
+	/**
+	 * Break lock
+	 */
+	BreakHoveredLock() {
+		return game.LocknKey.BreakHoveredLock();
+	},
+
+	/** 
+	 * Pick lock
+	 */
+	PickHoveredLock() {
+		return game.LocknKey.PickHoveredLock();
+	},
+
+	/**
+	 * Pick pocket
+	 */
+	PickPocketHovered() {
+		return game.LocknKey.PickPocketHovered();
+	},
+
+	/**
+	 * Use custom check
+	 */
+	CustomCheckHoveredLock() {
+		return game.LocknKey.CustomCheckHoveredLock();
+	},
+
+	/**
+	 * Use Key
+	 */
+	UseKeyonSelectedLock() {
+		return game.LocknKey.UseKeyonSelectedLock();
 	}
 };
 
